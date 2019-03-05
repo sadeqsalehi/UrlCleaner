390697edc34732e44f72d626572aa613e7822795
Ninan <ninan@ninans-mac.local>
2019-03-05 09:33:17 -0800

    Initial Commit

---
+++ UrlCleaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UrlCleaner
+//
+//  Created by Ninan on 3/5/19.
+//  Copyright © 2019 Ninan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UrlCleaner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UrlCleaner
+//
+//  Created by Ninan on 3/5/19.
+//  Copyright © 2019 Ninan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
