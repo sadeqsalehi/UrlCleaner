d90d7db97ece43510cba0263ba71796e2c3fcb2f
Ninan <ninan@ninans-mac.local>
2019-03-05 14:24:04 -0800

    Copy, Paste, Cleanup Code

--- UrlCleaner/ViewController.swift
+++ UrlCleaner/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var chkDots: NSButtonCell!
     
+    @IBAction func buttonDidClick(_ sender: NSButton) {
+        let button = sender as NSButton
+        debugPrint(button.title)
+        checkWichBittonClicked(button.title)
+        
+    }
     @IBAction func checkBoxDidClick(_ sender: NSButtonCell) {
         let chkbox = sender as NSButtonCell
         checkStatevalues(chkbox.tag)
@@ -25,7 +31,50 @@ class ViewController: NSViewController {
         debugPrint(UrlAddress.specialChars)
         
     }
-    private func checkStatevalues(_ tag:Int){
+    private func checkWichBittonClicked(_ title:String)->Void{
+        switch title {
+        case "Copy":
+            pasteboardClearContents()
+            setPasteboardItems(txtUrl.stringValue)
+        case "Paste":
+            txtUrl.stringValue = pasteboardStringContent()
+        case "Clean Up":
+          txtUrl.stringValue  =   UrlAddress.cleanUpUrl(txtUrl.stringValue, UrlAddress.specialChars)
+            
+        default:
+            break
+        }
+    }
+    private func setPasteboardItems(_ item: String)->Void{
+        let pasteboard = NSPasteboard.general
+        pasteboard.declareTypes([NSPasteboard.PasteboardType.string], owner: nil)
+        pasteboard.setString(item, forType: NSPasteboard.PasteboardType.string)
+        var clipboardItems: [String] = []
+        for element in pasteboard.pasteboardItems! {
+            if let str = element.string(forType: NSPasteboard.PasteboardType(rawValue: "public.utf8-plain-text")) {
+                clipboardItems.append(str)
+                if clipboardItems.count>0 {
+                    debugPrint(clipboardItems)
+                }
+            }
+        }
+    }
+    func pasteboardStringContent() -> String
+    {
+        if NSPasteboard.general.pasteboardItems!.count > 0 {
+            return (NSPasteboard.general.pasteboardItems?.first?.string(forType: .string))!
+        }
+        else{
+            return ""
+        }
+        // return NSPasteboard.general.pasteboardItems?.first?.string(forType: .string)
+    }
+    func pasteboardClearContents()->Void{
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        
+    }
+    private func checkStatevalues(_ tag:Int)->Void{
         switch tag {
         case 1:
             if chkHypens.state.rawValue == 1 {
@@ -54,9 +103,7 @@ class ViewController: NSViewController {
         default:
             break
         }
-        
-        
-    }
+     }
     override func viewDidLoad() {
         super.viewDidLoad()
         txtUrl.stringValue = UrlAddress.cleanUpUrl("http://www.soft98.ir/the-movie_name+size.extension-.mkv")
